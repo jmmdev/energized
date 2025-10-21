9c953fdec50dd8c2b0e8a3318813a4ac979e967e
devjosm <devjosm@gmail.com>
2025-10-21 09:34:27 +0200

    Side Menu Section Bugfix

--- src/components/compact-login.js
+++ src/components/compact-login.js
@@ -11,57 +11,37 @@ export default function CompactLogin({onLoginSuccess, vertical}) {
     const [user, setUser] = useState('');
     const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState('');
-    const [targetElement, setTargetElement] = useState(null);
-    const [isLogging, setIsLogging] = useState(false);
 
     useEffect(() => {
         setErrMsg('');
     }, [user, pwd])
 
     useEffect(() => {
-        setIsLogging(false);
         errMsg.length > 0 && alert(errMsg);
     }, [errMsg])
 
-    useEffect(() => {
-        if (targetElement)
-            setIsLogging(true);
-    }, [targetElement])
-    
-    useEffect(() => {
-        const handleFormSubmit = async () => {
-            try {
-                const formData = new FormData(targetElement);
-                const response = await doCredentialsLogin(formData);
+    const handleFormSubmit = async (event) => {
+        try {
+            const formData = new FormData(event.currentTarget);
+            const response = await doCredentialsLogin(formData);
 
-                if (!!response.error) {
-                    setErrMsg("Login failed. Please check your credentials and try again.");
-                } else {
-                    await onLoginSuccess();
-                }
-            }
-            catch (err) {
+            if (!!response.error) {
                 setErrMsg("Login failed. Please check your credentials and try again.");
+            } else {
+                await onLoginSuccess();
             }
         }
-
-        if (isLogging) {
-            handleFormSubmit();
+        catch (err) {
+            setErrMsg("Login failed. Please check your credentials and try again.");
         }
-    }, [isLogging])
+    }
 
-    
-    
     return (
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            setTargetElement(e.currentTarget);
-        }} action="/api/auth/callback/credentials"
-        className={`flex ${vertical ? "w-full flex-col" : ""} gap-4 items-center text-container ${isLogging && "opacity-60"}`}>
+        <form onSubmit={handleFormSubmit} action="/api/auth/callback/credentials"
+        className={`flex ${vertical ? "w-full flex-col" : ""} gap-4 items-center text-container`}>
             <div className={`${vertical && "w-full"} flex gap-2 items-center`}>
                 <FaUser className="text-xl text-neutral-400" />
                 <input className={`${vertical && "w-full"} bg-my-white text-my-black rounded-xs border border-container`}
-                    disabled={isLogging}
                     placeholder="Username or email"
                     type="text"
                     name="identifier"
@@ -71,7 +51,6 @@ export default function CompactLogin({onLoginSuccess, vertical}) {
             <div className={`${vertical && "w-full"} flex gap-2 items-center`}>
                 <FaKey className="text-xl text-neutral-400" />
                 <input className={`${vertical && "w-full"} bg-my-white text-my-black rounded-xs border border-container`}
-                    disabled={isLogging}
                     placeholder="Password"
                     type="password"
                     name="password"
@@ -82,10 +61,10 @@ export default function CompactLogin({onLoginSuccess, vertical}) {
             <div className={`flex ${vertical && "flex-col w-full"} items-center gap-2`}>
                 <Button color="blue"
                 className={`rounded-xs px-2 text-my-white ${!(user.length > 0 && pwd.length > 0) ? "opacity-50" : "opacity-100"} ${vertical && "w-full"}`}
-                disabled={user.length <= 0 || pwd.length <= 0 || isLogging} onClick={() => {}}>
+                disabled={!(user.length > 0 && pwd.length > 0)} onClick={() => {}}>
                     Log in
                 </Button>
-                <Button color="gray" className={`rounded-xs text-my-white px-2 ${vertical && "w-full"}`} disabled={isLogging} onClick={(e) => {
+                <Button color="gray" className={`rounded-xs text-my-white px-2 ${vertical && "w-full"}`} onClick={(e) => {
                     e.preventDefault();
                     router.push("/register");
                 }}>
